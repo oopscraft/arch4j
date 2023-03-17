1dc567fe02b74a0da3ea3908f0cbf98f7875830f
chomookun <chomookun@gmail.com>
2023-03-17 23:32:56 +0900

    fix project name to arch4j

---
+++ arch4j-api/src/main/java/org/oopscraft/app4j/api/ApiConfiguration.java
@@ -0,0 +1,12 @@
+package org.oopscraft.app4j.api;
+
+
+import org.oopscraft.app4j.core.CoreConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(CoreConfiguration.class)
+public class ApiConfiguration {
+
+}

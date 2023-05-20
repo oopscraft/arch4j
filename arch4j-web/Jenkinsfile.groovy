17bc115b5d56436797b6291226a0e0753e500bd1
chomookun <chomookun@gmail.com>
2023-05-20 14:22:32 +0900

    .

--- arch4j-web/Jenkinsfile.groovy
+++ arch4j-web/Jenkinsfile.groovy
@@ -13,12 +13,12 @@ pipeline {
                 description: 'publishing maven credentials')
         string(name: 'JIB_FROM_IMAGE', defaultValue: params.JIB_FROM_IMAGE, description: 'container base image')
         credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl',
-                name: 'JIB_FROM_CREDENTIALS',
+                name: 'JIB_FROM_AUTH_CREDENTIALS',
                 defaultValue: params.JIB_FROM_CREDENTIALS,
                 description: 'base image repository credentials')
         string(name: 'JIB_TO_IMAGE', defaultValue: params.JIB_TO_IMAGE, description: 'target image')
         credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl',
-                name: 'JIB_TO_CREDENTIALS',
+                name: 'JIB_TO_AUTH_CREDENTIALS',
                 defaultValue: params.JIB_TO_CREDENTIALS,
                 description: 'target image repository credentials')
     }
@@ -35,14 +35,13 @@ pipeline {
                 cleanWs()
                 checkout scm
                 sh '''
-                ./gradlew :arch4j-web:publish -x test --refresh-dependencies \
+                ./gradlew :arch4j-web:publish -x test --refresh-dependencies --stacktrace \
                 -PmavenUrl=${MAVEN_URL} \
                 -PmavenUsername=${MAVEN_CREDENTIALS_USR} \
                 -PmavenPassword=${MAVEN_CREDENTIALS_PWD} \
                 -PpublishingMavenUrl=${PUBLISHING_MAVEN_URL} \
                 -PpublishingMavenUsername=${PUBLISHING_MAVEN_CREDENTIALS_USR} \
                 -PpublishingMavenPassword=${PUBLISHING_MAVEN_CREDENTIALS_PSW} \
-                -x test --refresh-dependencies --stacktrace 
                 '''.stripIndent()
             }
         }
@@ -54,21 +53,22 @@ pipeline {
                 cleanWs()
                 checkout scm
                 sh '''
-                ./gradlew :arch4j-web:jib -x test --refresh-dependencies \
-                -DincludeSubmodule=true \
-                -PimagePublishHost=${IMAGE_PUBLISH_HOST} \
-                -PimageUsername=${IMAGE_CREDENTIALS_USR} \
-                -PimagePassword=${IMAGE_CREDENTIALS_PSW} \
-                -PimageTag=${IMAGE_TAG} \
-                ${GRADLE_EXTRA_OPTION}
+                ./gradlew :arch4j-web:jib -x test --refresh-dependencies --stacktrace \
+                -PjibFromImage=${JIB_FROM_IMAGE} \
+                -PjibFromAuthUsername=${JIB_FROM_AUTH_CREDENTIALS_USR} \
+                -PjibFromAuthPassword=${JIB_FROM_AUTH_CREDENTIALS_PSW} \
+                -PjibToImage=${JIB_TO_IMAGE} \
+                -PjibToTags=${JIB_TO_TAGS} \
+                -PjibToAuthUsername=${JIB_TO_AUTH_CREDENTIALS_USR} \
+                -PjibToAuthPassword=${JIB_TO_AUTH_CREDENTIALS_PSW} \
                 '''.stripIndent()
             }
         }
-        stage("rollout") {
-            steps {
-                sh("kubectl rollout restart deployment/apps-web")
-                sh("kubectl rollout status deployment/apps-web")
-            }
-        }
+//        stage("rollout") {
+//            steps {
+//                sh("kubectl rollout restart deployment/apps-web")
+//                sh("kubectl rollout status deployment/apps-web")
+//            }
+//        }
     }
 }

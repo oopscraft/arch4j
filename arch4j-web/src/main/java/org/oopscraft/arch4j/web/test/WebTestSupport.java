6186223cca5aa32ed665ed3474f6856bf4ce9e4f
chomookun <chomookun@gmail.com>
2023-06-08 19:38:05 +0900

    add @WithUserDetails for web test

--- arch4j-web/src/main/java/org/oopscraft/arch4j/web/test/WebTestSupport.java
+++ arch4j-web/src/main/java/org/oopscraft/arch4j/web/test/WebTestSupport.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mockito;
+import org.oopscraft.arch4j.core.security.UserDetailsImpl;
 import org.oopscraft.arch4j.web.WebApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @SpringBootTest(classes = WebApplication.class)
 @AutoConfigureMockMvc
-@WithMockUser(username = "test", roles = {"TEST"})
+@WithUserDetails(value="admin")
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Transactional

a1aca015c4110768f8051a586560ce3d227c997d
chomookun <chomookun@gmail.com>
2023-07-03 01:14:48 +0900

    updates duice js

--- arch4j-web/src/main/resources/static/duice/duice.js
+++ arch4j-web/src/main/resources/static/duice/duice.js
@@ -2090,7 +2090,6 @@ var duice;
              * @param dialogElement
              */
             constructor(dialogElement) {
-                this.closeButtonImg = 'data:image/svg+xml;base64,' + window.btoa('<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 6L18 18M18 6L6 18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>');
                 this.dialogElement = dialogElement;
                 let _this = this;
                 // dialog fixed style
@@ -2127,8 +2126,8 @@ var duice;
                     };
                 };
                 // creates close button
-                this.closeButton = document.createElement('img');
-                this.closeButton.src = this.closeButtonImg;
+                this.closeButton = document.createElement('span');
+                this.closeButton.appendChild(document.createTextNode('X'));
                 this.closeButton.style.position = 'absolute';
                 this.closeButton.style.top = '5px';
                 this.closeButton.style.right = '5px';

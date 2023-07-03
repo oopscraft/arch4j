b48dffd428d66f30a1fed27d7d0e6a6375eebbd5
chomookun <chomookun@gmail.com>
2023-07-03 23:27:30 +0900

    1. makes login page to be applied theme. 2. css s-direction-column to s-flex-direction-column

---
+++ arch4j-web/src/main/java/org/oopscraft/arch4j/web/login/JoinController.java
@@ -0,0 +1,23 @@
+package org.oopscraft.arch4j.web.login;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("login/join")
+@RequiredArgsConstructor
+public class JoinController {
+
+    /**
+     * index
+     * @return model and view
+     */
+    @GetMapping
+    public ModelAndView index() {
+        return new ModelAndView("login/join.html");
+    }
+
+}

cc4fd4387efbbaefaf32fbfb1ba6297fcc93d6e2
chomookun <chomookun@gmail.com>
2024-02-09 16:30:06 +0900

    update duice-plugin/duice-pagination.js

--- arch4j-web/src/main/resources/static/duice-plugin/duice-pagination.js
+++ arch4j-web/src/main/resources/static/duice-plugin/duice-pagination.js
@@ -6,37 +6,41 @@ var duice;
         class Pagination extends duice.CustomElement {
             constructor(htmlElement, bindData, context) {
                 super(htmlElement, bindData, context);
+                this.prevContent = '<';
+                this.nextContent = '>';
+                this.pageNumberSize = 10;
                 // attributes
                 this.pageProperty = duice.getElementAttribute(htmlElement, 'page-property');
                 this.sizeProperty = duice.getElementAttribute(htmlElement, 'size-property');
                 this.countProperty = duice.getElementAttribute(htmlElement, 'count-property');
                 this.onclick = new Function(duice.getElementAttribute(htmlElement, 'onclick'));
+                // optional
+                this.pageNumberSize = Number(duice.getElementAttribute(htmlElement, 'page-number-size') || this.pageNumberSize);
+                this.prevContent = duice.getElementAttribute(htmlElement, 'prev-content') || this.prevContent;
+                this.nextContent = duice.getElementAttribute(htmlElement, 'next-content') || this.nextContent;
             }
             doRender(object) {
-                // optional
-                let prevText = duice.getElementAttribute(this.getHtmlElement(), 'prev-text') || '<︎';
-                let nextText = duice.getElementAttribute(this.getHtmlElement(), 'next-text') || '>︎';
                 // page,size,count
                 let page = Number(object[this.pageProperty]);
                 let size = Number(object[this.sizeProperty]);
                 let count = Number(object[this.countProperty]);
                 // calculate page
                 let totalPage = Math.ceil(count / size);
-                let startPageIndex = Math.floor(page / 10) * 10;
-                let endPageIndex = Math.min(startPageIndex + 9, totalPage - 1);
+                let startPageIndex = Math.floor(page / this.pageNumberSize) * this.pageNumberSize;
+                let endPageIndex = Math.min(startPageIndex + (this.pageNumberSize - 1), totalPage - 1);
                 endPageIndex = Math.max(endPageIndex, 0);
                 // template
                 let pagination = document.createElement('ul');
                 pagination.classList.add(`${duice.getNamespace()}-pagination`);
                 // prev
                 let prev = document.createElement('li');
-                prev.appendChild(document.createTextNode(prevText));
+                prev.innerHTML = this.prevContent;
                 prev.classList.add(`${duice.getNamespace()}-pagination__item-prev`);
-                prev.dataset.page = String(Math.max(startPageIndex - 10, 0));
+                prev.dataset.page = String(Math.max(startPageIndex - this.pageNumberSize, 0));
                 prev.addEventListener('click', () => {
                     this.onclick.call(prev);
                 });
-                if (page < 10) {
+                if (page < this.pageNumberSize) {
                     prev.classList.add(`${duice.getNamespace()}-pagination__item--disable`);
                 }
                 pagination.appendChild(prev);
@@ -56,7 +60,7 @@ var duice;
                 }
                 // next
                 let next = document.createElement('li');
-                next.appendChild(document.createTextNode(nextText));
+                next.innerHTML = this.nextContent;
                 next.classList.add(`${duice.getNamespace()}-pagination__item-next`);
                 next.dataset.page = String(Math.min(endPageIndex + 1, totalPage));
                 next.addEventListener('click', () => {

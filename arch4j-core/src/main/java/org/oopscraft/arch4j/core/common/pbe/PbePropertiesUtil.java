19f0d7fd84a89ff37105e68a52720223e82f7ee3
chomookun <chomookun@gmail.com>
2024-10-21 21:25:52 +0900

    PbePropertiesUtil - adds comment

--- arch4j-core/src/main/java/org/oopscraft/arch4j/core/common/pbe/PbePropertiesUtil.java
+++ arch4j-core/src/main/java/org/oopscraft/arch4j/core/common/pbe/PbePropertiesUtil.java
@@ -10,6 +10,11 @@ import java.util.Properties;
 
 public class PbePropertiesUtil {
 
+    /**
+     * encodes properties string with pbe encryption
+     * @param propertiesString properties string
+     * @return encoded properties string
+     */
     public static String encodePropertiesString(@NotNull String propertiesString) {
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -37,6 +42,11 @@ public class PbePropertiesUtil {
         return stringBuilder.toString();
     }
 
+    /**
+     * loads properties from encrypted properties string
+     * @param propertiesString properties string
+     * @return properties
+     */
     public static Properties loadProperties(String propertiesString) {
         Properties properties = new Properties();
         if (propertiesString != null && propertiesString.trim().length() > 0) {
@@ -63,112 +73,5 @@ public class PbePropertiesUtil {
         });
         return newProperties;
     }
-
-//    private static Properties processProperties(Properties properties, Function<String, String> processFunction) {
-//        Properties newProperties = new Properties();
-//        properties.forEach((keyObject, valueObject) -> {
-//            String key = (String) keyObject;
-//            String value = Optional.ofNullable(valueObject)
-//                    .map(Object::toString)
-//                    .orElse(null);
-//            value = processFunction.apply(value);
-//            newProperties.setProperty(key, value);
-//        });
-//        return newProperties;
-//    }
-//
-//    private static String processProperties(String properties, Function<String,String> processFunction) {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        try (StringReader stringReader = new StringReader(properties)) {
-//            List<String> lines = IOUtils.readLines(stringReader);
-//            for(String line : lines) {
-//                if(line.isBlank() || line.startsWith("#")) {
-//                    stringBuilder.append(line)
-//                            .append('\n');
-//                    continue;
-//                }
-//                int indexOf = line.indexOf("=");
-//                String key = line.substring(0, indexOf);
-//                String value = line.substring(indexOf+1);
-//                value = processFunction.apply(value);
-//                stringBuilder.append(key)
-//                        .append("=")
-//                        .append(value)
-//                        .append('\n');
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return stringBuilder.toString();
-//    }
-//
-//    public static Properties encodeAsProperties(Properties properties) {
-//        return processProperties(properties, PbeStringUtil::encode);
-//    }
-//
-//    public static Properties encodeAsProperties(String propertiesString) {
-//        return encodeAsProperties(stringToProperties(propertiesString));
-//    }
-//
-//    public static String encodeAsString(String properties) {
-//        return processProperties(properties, PbeStringUtil::encode);
-//    }
-//
-//    public static String encodedAsString(Properties properties) {
-//        return encodeAsString(propertiesToString(properties));
-//    }
-//
-//    public static Properties decodeAsProperties(Properties properties) {
-//        return processProperties(properties, PbeStringUtil::decode);
-//    }
-//
-//    public static Properties decodeAsProperties(String propertiesString) {
-//        return decodeAsProperties(stringToProperties(propertiesString));
-//    }
-//
-//    public static String decodeAsString(String properties) {
-//        return processProperties(properties, PbeStringUtil::decode);
-//    }
-//
-//    public static String decodeAsString(Properties properties) {
-//        return decodeAsString(propertiesToString(properties));
-//    }
-//
-////    public static Properties unwrapEncryptedMark(Properties properties) {
-////        return processProperties(properties, PbeStringUtil::unwrapEncryptedMark);
-////    }
-////
-////    public static String unwrapEncryptedMark(String properties) {
-////        return processProperties(properties, PbeStringUtil::unwrapEncryptedMark);
-////    }
-////
-////    public static Properties unwrapDecryptedMark(Properties properties) {
-////        return processProperties(properties, PbeStringUtil::unwrapDecryptedMark);
-////    }
-////
-////    public static String unwrapDecryptedMark(String properties) {
-////        return processProperties(properties, PbeStringUtil::unwrapDecryptedMark);
-////    }
-//
-//    private static String propertiesToString(Properties properties) {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        properties.forEach((key, value) -> {
-//            stringBuilder.append(key)
-//                    .append("=")
-//                    .append(value)
-//                    .append('\n');
-//        });
-//        return stringBuilder.toString();
-//    }
-//
-//    private static Properties stringToProperties(String propertiesString) {
-//        Properties properties = new Properties();
-//        try {
-//            properties.load(new StringReader(propertiesString));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return properties;
-//    }
 
 }

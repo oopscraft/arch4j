95a34360694c8015c722ee31b461a54366ba7bd8
chomookun <chomookun@gmail.com>
2025-01-26 22:45:05 +0900

    upgrades duice

--- arch4j-web/src/main/resources/static/duice/duice.js
+++ arch4j-web/src/main/resources/static/duice/duice.js
@@ -1,5 +1,5 @@
 /*!
- * duice - v0.2.43
+ * duice - v0.2.44
  * git: https://gitbub.com/chomookun/duice
  * website: https://duice.chomookun.com
  * Released under the LGPL(GNU Lesser General Public License version 3) License
